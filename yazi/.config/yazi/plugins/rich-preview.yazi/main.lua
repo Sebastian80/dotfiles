a853e44a33d2090aff8785e44fe07b01b2407301
Sebastian <sebastian.ertner@netresearch.de>
2026-09-07 22:41:58 +0200

    fix(yazi): rich-preview uses ui.Wrap.YES/NO, ui.Text.WRAP is gone in yazi 26

--- yazi/.config/yazi/plugins/rich-preview.yazi/main.lua
+++ yazi/.config/yazi/plugins/rich-preview.yazi/main.lua
@@ -43,7 +43,7 @@ function M:peek(job)
 	else
 		lines = lines:gsub("\t", string.rep(" ", rt.preview.tab_size))
 		ya.preview_widgets(job, {
-			ui.Text.parse(lines):area(job.area):wrap(rt.preview.wrap == "yes" and ui.Text.WRAP or ui.Text.WRAP_NO),
+			ui.Text.parse(lines):area(job.area):wrap(rt.preview.wrap == "yes" and ui.Wrap.YES or ui.Wrap.NO),
 		})
 	end
 end
